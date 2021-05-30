a90373b6e59c153d7defb6b2a96244b4dd473706
Andreas <andreas.matthias@gmail.com>
2021-05-30 23:36:14 +0200

    Travis CI integration.

---
+++ scripts/texlive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Installing TeX Live for Travis-CI
+
+export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+
+# Get TeX Live if not cached already
+if ! command -v luatex >/dev/null 2>&1; then
+    wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+    tar -xzf install-tl-unx.tar.gz
+    cd install-tl-20*
+    ./install-tl --profile=../scripts/texlive.profile
+    cd ..
+fi
+
+# update self
+tlmgr update --self
+
+# install packages
+tlmgr install \
+      luatex \
+      kvsetkeys \
+      etoolbox
+
+# no backups
+tlmgr option -- autobackup 0
+
+# update
+tlmgr update --all --no-auto-install
